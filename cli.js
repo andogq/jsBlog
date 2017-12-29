f8a68210acf86cdcad83fb1b916a055f4dbb7162
tehlordvortex <hadokoda@yahoo.com>
2017-12-30 00:57:18 +0100

    implement interactive mode

--- cli.js
+++ cli.js
@@ -1,5 +1,8 @@
 const fs = require("fs");
 const path = require("path");
+const readline = require("readline");
+
+let rl = null; 
 
 let title = "";
 let date = "";
@@ -8,20 +11,61 @@ let link = "";
 const regexNumber = /^\d+/;
 const regexNonAlphanum = /\W+/g;
 
+function generateDate() {
+    let cDate = new Date();
+    return cDate.getDate() + "/" + cDate.getMonth() + "/" + cDate.getFullYear().toString().substr(2); // dd/mm/yy
+}
+
+function generateLinkFromTitle(title) {
+    return title.toLowerCase().replace(regexNonAlphanum, "-") + ".md";
+}
+
 function generateEntry() {
     entry = {
         title: title,
         date: date,
         link: link
-    }
-    let posts = require(path.join(__dirname, "posts/postList.json"))
-    posts.push(entry)
-    fs.writeFileSync(path.join(__dirname, "posts/postList.json"), JSON.stringify(posts, null, 4))
+    };
+    let posts = require(path.join(__dirname, "posts/postList.json"));
+    posts.push(entry);
+    fs.writeFileSync(path.join(__dirname, "posts/postList.json"), JSON.stringify(posts, null, 4));
     fs.writeFileSync(path.join(__dirname, "posts/" + link), "# " + title + "\n")
-    console.log("Fire up your editor and edit posts/" + link)
+    console.log("Fire up your editor and edit posts/" + link);
 }
+
+function requestTitle() {
+    rl.question('Title: ', (t) => {
+        if (!t) requestTitle();
+        else {
+            title = t;
+            requestDate();
+        }
+    });
+}
+
+function requestDate() {
+    let genDate = generateDate();
+    rl.question('Date('+genDate+'): ', (d) => {
+        date = d || genDate;
+        requestLink();
+    });
+}
+
+function requestLink() {
+    let genLink = generateLinkFromTitle(title);
+    rl.question('Link('+genLink+'): ', (l) => {
+        link = l || genLink;
+        generateEntry();
+        rl.close();
+    });
+}
+
 function interactiveMode() {
-    throw "Unimplemented";
+    rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+    requestTitle();
 }
 function cliMode() {
     if (process.argv[2] === "-h" || process.argv[2] === "--help") {
@@ -32,11 +76,10 @@ function cliMode() {
     console.log("Title: " + title);
     if (process.argv[3] && !regexNumber.test(process.argv[3])) date = process.argv[3]
     else {
-        let cDate = new Date();
-        date = cDate.getDate() + "/" + cDate.getMonth() + "/" + cDate.getFullYear().toString().substr(2) // dd/mm/yy
+        date = generateDate()
         link = process.argv[3];
     }
-    if (!link) link = title.toLowerCase().replace(regexNonAlphanum, "-") + ".md";
+    if (!link) link = generateLinkFromTitle(title); 
     console.log("Date: " + date)
     console.log("Link: " + link)
     generateEntry();
